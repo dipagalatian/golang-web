494119d26edf7e7b53b8eff80bcde846c33c6201
dipagalatian <dipagalatian3003@gmail.com>
2026-01-20 21:53:25 +0700

    FEAT: Add endpoints with using wildcard

--- handler_test.go
+++ handler_test.go
@@ -28,3 +28,48 @@ func TestHandlerServer(t *testing.T) {
 	}
 	
 }
+
+func TestServerMux(t *testing.T) {
+
+	// create server mux
+	// to handle multiple route
+	// like router in other framework
+	// NewServeMux is to create multiple handlerFunc or handler
+	// NewServeMux also implements the Handler interface like HandlerFunc
+	mux := http.NewServeMux()
+
+	// register route and handler func
+	
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello from root")
+	})
+
+	mux.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprint(w, "Hello from hi")
+	})
+
+	// endpoints with "/" at the end will match all subpaths
+	// for example: /images/thumbnails, /images/anything
+	// this is similar to using wildcard in other framework
+	// the longest pattern match will be used not the first one defined
+	// the order of definition does not matter in go net/http
+	
+	mux.HandleFunc("/images/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Images")
+	})
+
+	mux.HandleFunc("/images/thumnails/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Thumnails")
+	})
+
+	server := http.Server{
+		Addr: "localhost:8080",
+		Handler: mux,
+	}
+	
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+	
+}
