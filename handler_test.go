e0ae32a2dff0c6e9bfe9fedfbefadd0e6f42bc2b
dipagalatian <dipagalatian3003@gmail.com>
2026-01-20 22:16:45 +0700

    FEAT: Access http request from pointer http.Request in handler

--- handler_test.go
+++ handler_test.go
@@ -73,3 +73,23 @@ func TestServerMux(t *testing.T) {
 	}
 	
 }
+
+func TestRequest(t *testing.T) {
+
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, r.Method)
+		fmt.Fprintln(w, r.Host)
+		fmt.Fprintln(w, r.RequestURI)
+	}
+
+	server := http.Server{
+		Addr: "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+	
+}
